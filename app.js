bfa37ec10ede0f6bfbafca33439ddb55c467be03
James Ma <james.sh.ma@gmail.com>
2017-07-18 14:41:01 -0400

    web application structure

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const routes = require('./router');
+
+const app = express();
+
+
+
+//morgan logs the requests that are being made to the server
+app.use(morgan('dev'));
+
+//this tells express to look in the public folder for required files
+app.use(express.static(__dirname + '/public'));
+
+//this tells express where to look to interpret the routes
+app.use(routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500).send(err.message);
+});
+
+
+//starts the server
+app.listen(1337, function(){
+	console.log(chalk.green('listening on port 1337'));
+});
